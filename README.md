19dc1a391992351a737b00a06d2dc62a18cabbb7
Tayerquach <quachmaiboi@gmail.com>
2023-12-30 12:41:19 +0000

    add official files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DERCo
+python3 run_raw_epoch.py -subject_id=ACB71 -topic_id=0
--- old20/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Calculate the old_n."""
-from old20.old20 import old_n, old20, old_subloop
-
-__all__ = ["old_n", "old20", "old_subloop"]
