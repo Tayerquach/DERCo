6454774cd3e7099e59a1a98a3e919082c345054d
Tayerquach <39531275+Tayerquach@users.noreply.github.com>
2024-02-22 16:08:59 +0000

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,8 +1,8 @@
 settings = dict(
     log=dict(author="Boi Mai Quach"),
-    bandpass_filter=dict(low_freq=0.5, high_freq=45),
+    bandpass_filter=dict(low_freq=0.1, high_freq=45),
     notch_filter = dict(line_freq=50),
     epochs=dict(start_time=-0.2, end_time=1.000, duration=1),
     ica=dict(ica_n_components = .99, method="picard", decim=None),
-    autoreject=dict(threshold=0.15),
+    autoreject=dict(threshold=0.25),
 )
